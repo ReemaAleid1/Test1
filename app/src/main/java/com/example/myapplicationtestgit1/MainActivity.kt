5656f54ea65fee4628fd818dd71cd4c1232ff371
reema-aleid <438201181@student.ksu.edu.sa>
2022-12-22 11:37:54 +0300

    this is first commit

---
+++ app/src/main/java/com/example/myapplicationtestgit1/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplicationtestgit1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // this is just for test push the project
+        println("Hello World")
+
+    }
+
+}
